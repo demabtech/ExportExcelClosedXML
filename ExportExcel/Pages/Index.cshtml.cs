f39180bb647b8ffc7d543b3ef3eabff561300a34
Carlos Dema <carlosdema@demabtech.com>
2020-07-11 17:08:07 -0500

    Add project to Git

---
+++ ExportExcel/Pages/Index.cshtml.cs
@@ -0,0 +1,66 @@
+﻿using ClosedXML.Excel;
+using ExportExcel.Models;
+using ExportExcel.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExportExcel.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public IEnumerable<Product> _products;
+
+        public IndexModel(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public void OnGet()
+        {
+            _products = ProductService.Get();
+        }
+
+        public IActionResult OnPostDownload()
+        {
+            string fileName = $"Products-{DateTime.Now.ToString("dd-MM-yyyy-hh-ss")}.xlsx";
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Listado de Productos");
+                worksheet.Cell(1, 1).Value = "Id";
+                worksheet.Cell(1, 2).Value = "Nombre";
+                worksheet.Cell(1, 3).Value = "Descripción";
+                worksheet.Cell(1, 4).Value = "Stock";
+                worksheet.Cell(1, 5).Value = "Precio";
+                worksheet.Row(1).Style.Fill.BackgroundColor = XLColor.BlueGray;
+                worksheet.Row(1).Style.Font.Bold = true;
+
+                var products = ProductService.Get();
+                int row = 2;
+                foreach (var product in products)
+                {
+                    worksheet.Cell(row, 1).Value = product.Id;
+                    worksheet.Cell(row, 2).Value = product.Name;
+                    worksheet.Cell(row, 3).Value = product.Description;
+                    worksheet.Cell(row, 4).Value = product.Stock;
+                    worksheet.Cell(row, 5).Value = product.Price.ToString("C");
+                    row++;
+                }
+
+                worksheet.Columns().AdjustToContents();
+                workbook.SaveAs(fileName);
+            }
+
+            var contentRoot = _webHostEnvironment.ContentRootPath;
+            string path = Path.Combine(contentRoot, fileName);
+            var fileStream = System.IO.File.OpenRead(path);
+            return File(fileStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+        }
+    }
+}
---
+++ ExportExcel/Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using ExportExcel.Models;
+using System.Collections.Generic;
+
+namespace ExportExcel.Services
+{
+    public class ProductService
+    {
+        public static IEnumerable<Product> Get() =>
+            new List<Product>()
+            {
+                new Product{Id = 1, Name="Portátil HP Envy Laptop", Description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus bibendum venenatis. Nullam id nisl et enim egestas porta.", Stock = 2, Price = 700 },
+                new Product{Id = 2, Name="PC Gamer Asus TUF Gaming", Description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus bibendum venenatis. Nullam id nisl et enim egestas porta.", Stock = 1, Price = 900 },
+                new Product{Id = 3, Name="Ipad séptima generación 10.2", Description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus bibendum venenatis. Nullam id nisl et enim egestas porta.", Stock = 4, Price = 500 },
+                new Product{Id = 4, Name="Multifuncional Ecotank L3110", Description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus bibendum venenatis. Nullam id nisl et enim egestas porta.", Stock = 3, Price = 200 },
+                new Product{Id = 5, Name="Mouse R.A.T 8+ GAMING Negro", Description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus bibendum venenatis. Nullam id nisl et enim egestas porta.", Stock = 5, Price = 50 },
+                new Product{Id = 6, Name="Control Dualshock4 Sony", Description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus bibendum venenatis. Nullam id nisl et enim egestas porta.", Stock = 8, Price = 150 },
+                new Product{Id = 7, Name="Control inalámbrico para xbox 360-negro", Description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus bibendum venenatis. Nullam id nisl et enim egestas porta.", Stock = 2, Price = 130 },
+                new Product{Id = 8, Name="Audífonos HyperX HX-HSCSC2", Description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus bibendum venenatis. Nullam id nisl et enim egestas porta.", Stock = 20, Price = 70 },
+                new Product{Id = 9, Name="Vídeo Juego Portátil - Retro 400", Description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus bibendum venenatis. Nullam id nisl et enim egestas porta.", Stock = 12, Price = 40 },
+            };
+    }
+}
